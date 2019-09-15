1096a7f03d9aa5d16bcbfb1940c73936ae84f787
Michael Rondon <m>
2019-09-15 13:09:14 +0200

    Assignment 1: Practicing Components

---
+++ assignment1/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: '<div class="alert alert-warning" role="alert">warning-alert works!</div>'
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
