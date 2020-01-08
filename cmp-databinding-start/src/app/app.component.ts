0ec014b488fc01ae91ae5c24cb738752bf3ca4b5
mrondon <mrondon-externo@cnmc.es>
2020-01-08 14:58:21 +0100

    63. Module Introduction

---
+++ cmp-databinding-start/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  newServerContent = '';
+
+  newServerName = '';
+
+  serverElements = [];
+
+  onAddServer() {
+
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+
+  }
+
+  onAddBlueprint() {
+
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+    
+  }
+}
