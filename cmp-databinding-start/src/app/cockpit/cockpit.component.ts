878ccef7da3303611b7fdbcbe8992aa622339e6d
mrondon <mrondon-externo@cnmc.es>
2020-01-09 12:04:33 +0100

    68. Binding to custom events

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
+  @Output() blueprintCreated = new EventEmitter<{ serverName: string; serverContent: string }>();
+
   newServerContent = '';
 
   newServerName = '';
+
+  @Output() serverCreated = new EventEmitter<{ serverName: string; serverContent: string }>();
 
   constructor() { }
 
@@ -18,21 +22,19 @@ export class CockpitComponent implements OnInit {
 
   onAddServer() {
 
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
 
   }
 
   onAddBlueprint() {
 
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
 
   }
 
