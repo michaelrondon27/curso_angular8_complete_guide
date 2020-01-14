a331943589d1f3af15da2eb1d52a5772e0cc1d2d
mrondon <mrondon-externo@cnmc.es>
2020-01-14 16:17:57 +0100

    96. Using hostlistenr to listen to host events

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
-import { BetterHighlightDirective } from './better-highlight./better-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
