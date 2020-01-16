63c492985db40d2bf494ae3d92947a7342cd1cde
mrondon <mrondon-externo@cnmc.es>
2020-01-16 13:47:07 +0100

    121. Adding ingredients to recipes

--- course-project/src/app/recipes/recipe.model.ts
+++ course-project/src/app/recipes/recipe.model.ts
@@ -1,15 +1,20 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
     public name: string;
     public descriprion: string;
     public imagePath: string;
+    public ingredients: Ingredient[];
 
-    constructor(name: string, desc: string, imagePath: string) {
+    constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
 
         this.name = name;
 
         this.descriprion = desc;
 
         this.imagePath = imagePath;
+
+        this.ingredients = ingredients;
 
     }
 }
--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
     providedIn: 'root'
@@ -9,14 +10,22 @@ export class RecipeService {
 
     private recipes: Recipe[] = [
         new Recipe(
-          'A Test Recipe',
-          'This is simply a test',
-          'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+          'Tasty Schnitzel',
+          'A super-tasty schnitzel - just awesome!',
+          'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+          [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20),
+          ]
         ),
         new Recipe(
-          'Another Test Recipe',
-          'This is simply a test',
-          'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+          'Big Fat Burger',
+          'What else you need to say?',
+          'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+          [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1),
+          ]
         )
     ];
 
