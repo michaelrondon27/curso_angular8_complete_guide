0423a7670e50fafc3a19813019be86026d985aa9
mrondon <mrondon-externo@cnmc.es>
2020-01-17 13:47:25 +0100

    137. Retrieving query parameters and fragments

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -8,20 +9,44 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+
+  server: {
+    id: number,
+    name: string,
+    status: string
+  };
+
   serverName = '';
+
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+
+    console.log(this.activatedRoute.snapshot.queryParams);
+
+    console.log(this.activatedRoute.snapshot.fragment);
+
+    this.activatedRoute.queryParams.subscribe();
+
+    this.activatedRoute.fragment.subscribe();
+
     this.server = this.serversService.getServer(1);
+
     this.serverName = this.server.name;
+
     this.serverStatus = this.server.status;
+
   }
 
   onUpdateServer() {
+
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+
   }
 
 }
