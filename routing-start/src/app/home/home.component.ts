c201b1ae828c40def5b6a4130361f09e9170bbc9
mrondon <mrondon-externo@cnmc.es>
2020-01-17 13:43:24 +0100

    136. Passing query parameters and fragments

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -15,9 +15,14 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServer( id: number ) {
 
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id, 'edit'], {
+      queryParams: {
+        allowEdit: '1'
+      },
+      fragment: 'loading'
+    });
 
   }
 
