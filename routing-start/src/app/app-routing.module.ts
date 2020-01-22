4f68bd530ffac8f3a288a7e4df5474d7002c4f66
mrondon <mrondon-externo@cnmc.es>
2020-01-22 16:13:10 +0100

    150. Passing static data to a route

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth.guard";
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate.guard';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 const appRoutes: Routes = [
     {
@@ -49,9 +50,16 @@ const appRoutes: Routes = [
         }
       ]
     },
+    // {
+    //   path: 'not-found',
+    //   component: PageNotFoundComponent
+    // },
     {
       path: 'not-found',
-      component: PageNotFoundComponent
+      component: ErrorPageComponent,
+      data: {
+        message: 'Page not found!'
+      }
     },
     {
       path: '**',
