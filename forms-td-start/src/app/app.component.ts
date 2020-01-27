a01dbd23c1920ffe2c7c15444c5c5e46a2f26115
mrondon <mrondon-externo@cnmc.es>
2020-01-27 10:48:23 +0100

    197. TD: setting and patching form values

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -17,7 +17,25 @@ export class AppComponent {
   @ViewChild('f', {static: true}) signupForm: NgForm;
 
   suggestUserName() {
+
     const suggestedName = 'Superuser';
+
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+
   }
 
   // onSubmit( form: NgForm ) {
