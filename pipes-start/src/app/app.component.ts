e4de85b88c60c2845d76ad7de1ecb44f5e03167d
mrondon <mrondon-externo@cnmc.es>
2020-01-30 11:38:14 +0100

    247. Pure and impure pipes (or: how to fix the filter pipe)

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -45,4 +45,16 @@ export class AppComponent {
     };
 
   }
+
+  onAddServer() {
+
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    });
+
+  }
+
 }
