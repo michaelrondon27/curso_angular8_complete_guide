f129fcc573c3664f91b1a5febd850a42467cab68
mrondon <mrondon-externo@cnmc.es>
2020-02-06 10:34:17 +0100

    298. Creating & storing the user data

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+
+import { User } from './user.model';
 
 export interface AuthResponseData {
     kind: string;
@@ -18,6 +20,8 @@ export interface AuthResponseData {
 })
 export class AuthService {
 
+    user = new Subject<User>();
+
     constructor(
         private http: HttpClient
     ) {}
@@ -31,7 +35,19 @@ export class AuthService {
                 password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleError));
+        ).pipe(
+            catchError(this.handleError),
+            tap( resData => {
+
+                this.handleAuthentication(
+                    resData.email,
+                    resData.localId,
+                    resData.idToken,
+                    +resData.expiresIn
+                );
+
+            })
+        );
 
     }
 
@@ -44,7 +60,34 @@ export class AuthService {
                 password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleError));
+        ).pipe(
+            catchError(this.handleError),
+            tap( resData => {
+
+                this.handleAuthentication(
+                    resData.email,
+                    resData.localId,
+                    resData.idToken,
+                    +resData.expiresIn
+                );
+
+            })
+        );
+
+    }
+
+    private handleAuthentication( email: string, userId: string, token: string, expiresIn: number ) {
+
+        const expirationDate = new Date( new Date().getTime() + expiresIn + 1000);
+
+        const user = new User(
+            email,
+            userId,
+            token,
+            expirationDate
+        );
+
+        this.user.next(user);
 
     }
 
