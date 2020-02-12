4559c7f935737c8b1fbde6c03d81ee54d2ba3a15
mrondon <mrondon-externo@cnmc.es>
2020-02-12 17:28:11 +0100

    328. Adding an auth feature module

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
 import { HeaderComponent } from './header/header.component';
 
 // Routes
@@ -15,20 +13,19 @@ import { CoreModule } from './core.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
     HeaderComponent
   ],
   imports: [
     AppRoutingModule,
+    AuthModule,
     BrowserModule,
     CoreModule,
-    FormsModule,
     HttpClientModule,
-    ReactiveFormsModule,
     RecipesModule,
     SharedModule,
     ShoppingListModule
---
+++ course-project/src/app/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+// Components
+import { AuthComponent } from './auth.component';
+
+// Modules
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+    declarations: [
+        AuthComponent
+    ],
+    exports: [
+        AuthComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([
+            {
+                path: 'auth',
+                component: AuthComponent
+            }
+        ]),
+        SharedModule
+    ]
+})
+export class AuthModule {}
