50fea42b3fe8596279175c6307a585407d50704a
mrondon <mrondon-externo@cnmc.es>
2020-02-12 16:40:48 +0100

    321. Getting started with features modules

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -10,17 +10,12 @@ import { DropDownDirective } from './shared/dropdown.directive';
 import { HeaderComponent } from './header/header.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AuthInterceptor } from './auth/auth.interceptor';
+import { RecipesModule } from './recipes/recipes.module';
 
 
 @NgModule({
@@ -32,21 +27,16 @@ import { AuthInterceptor } from './auth/auth.interceptor';
     HeaderComponent,
     LoadingSpinnerComponent,
     PlaceholderDirective,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RecipesModule
   ],
   providers: [
     {
---
+++ course-project/src/app/recipes/recipes.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeDetailComponent,
+        RecipeEditComponent,
+        RecipeItemComponent,
+        RecipeListComponent,
+        RecipeStartComponent
+    ],
+    exports: [
+        RecipesComponent,
+        RecipeDetailComponent,
+        RecipeEditComponent,
+        RecipeItemComponent,
+        RecipeListComponent,
+        RecipeStartComponent
+    ]
+})
+export class RecipesModule {
+
+}
