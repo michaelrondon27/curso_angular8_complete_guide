da3c801ddeaae34f1ab8bc1ff1b33b5f56398134
mrondon <mrondon-externo@cnmc.es>
2020-02-13 13:44:39 +0100

    333. Preloading lazy loaded code

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const appRoutes: Routes = [
     {
@@ -23,7 +23,12 @@ const appRoutes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(
+            appRoutes,
+            {
+                preloadingStrategy: PreloadAllModules
+            }
+        )
     ],
     exports: [
         RouterModule
