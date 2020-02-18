109071fe89343aa9665eea95e791c99bdd10f243
mrondon <mrondon-externo@cnmc.es>
2020-02-18 20:12:42 +0100

    378. The router store

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -32,10 +33,11 @@ import { environment } from '../environments/environment';
     EffectsModule.forRoot([AuthEffects]),
     HttpClientModule,
     SharedModule,
-    StoreModule.forRoot(fromApp.appReducer),
+    StoreRouterConnectingModule.forRoot(),
     StoreDevtoolsModule.instrument({
       logOnly: environment.production
-    })
+    }),
+    StoreModule.forRoot(fromApp.appReducer)
   ],
   bootstrap: [AppComponent],
 })
