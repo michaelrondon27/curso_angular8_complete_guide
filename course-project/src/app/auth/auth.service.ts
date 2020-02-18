eb6c9b6a26055677ea80ad8372902a5c1d8c77ca
mrondon <mrondon-externo@cnmc.es>
2020-02-18 13:14:13 +0100

    362. Dispatching auth actions

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -2,9 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 import { User } from './user.model';
 import { Router } from '@angular/router';
+import * as fromApp from './../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 export interface AuthResponseData {
     kind: string;
@@ -27,7 +30,8 @@ export class AuthService {
 
     constructor(
         private http: HttpClient,
-        private router: Router
+        private router: Router,
+        private store: Store<fromApp.AppState>
     ) {}
 
     signup( email: string, password: string ) {
@@ -104,7 +108,12 @@ export class AuthService {
 
         if ( loadedUser.token ) {
 
-            this.user.next(loadedUser);
+            this.store.dispatch(new AuthActions.Login({
+                email: loadedUser.email,
+                userId: loadedUser.id,
+                token: loadedUser.token,
+                expirationDate: new Date(userData._tokenExpirationDate)
+            }));
 
             const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
 
@@ -116,7 +125,7 @@ export class AuthService {
 
     logout() {
 
-        this.user.next(null);
+        this.store.dispatch(new AuthActions.Logout());
 
         this.router.navigate(['/auth']);
 
@@ -153,7 +162,12 @@ export class AuthService {
             expirationDate
         );
 
-        this.user.next(user);
+        this.store.dispatch(new AuthActions.Login({
+            email,
+            userId,
+            token,
+            expirationDate
+        }));
 
         this.autoLogout( expiresIn * 1000 );
 
