c3cad8893d39d773a47d0bc182fda46ff9651d74
mrondon <mrondon-externo@cnmc.es>
2020-02-19 17:17:38 +0100

    458. Advanced transitions

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -14,8 +14,25 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         backgroundColor: 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
@@ -29,6 +46,8 @@ export class AppComponent {
 
   state = 'normal';
 
+  wildState = 'normal';
+
   onAdd( item ) {
 
     this.list.push( item );
@@ -39,11 +58,19 @@ export class AppComponent {
 
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
 
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+
   }
 
   onDelete( item ) {
 
     this.list.splice( this.list.indexOf(item), 1 );
+
+  }
+
+  onShrink() {
+
+    this.wildState = 'shrunken';
 
   }
 
